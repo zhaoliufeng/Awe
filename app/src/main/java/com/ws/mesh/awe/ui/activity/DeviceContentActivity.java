8d2d1ab33d24723f6f9e2a73ced0db24338d6b90
zhaoliufeng <zhaoliufeng4911@163.com>
2018-07-17 15:50:41 +0800

    完成路数适配

--- app/src/main/java/com/ws/mesh/awe/ui/activity/DeviceContentActivity.java
+++ app/src/main/java/com/ws/mesh/awe/ui/activity/DeviceContentActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import com.ws.mesh.awe.R;
 import com.ws.mesh.awe.base.BaseActivity;
 import com.ws.mesh.awe.base.BaseFragment;
+import com.ws.mesh.awe.bean.Device;
+import com.ws.mesh.awe.constant.DeviceChannel;
 import com.ws.mesh.awe.constant.IntentConstant;
 import com.ws.mesh.awe.ui.adapter.ViewPagerAdapter;
 import com.ws.mesh.awe.ui.fragment.ColourFragment;
@@ -14,6 +16,7 @@ import com.ws.mesh.awe.ui.fragment.ModesFragment;
 import com.ws.mesh.awe.ui.fragment.RoomsFragment;
 import com.ws.mesh.awe.ui.fragment.SceneFragment;
 import com.ws.mesh.awe.ui.fragment.SchedularFragment;
+import com.ws.mesh.awe.utils.CoreData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +45,8 @@ public class DeviceContentActivity extends BaseActivity {
     @Override
     protected void initData() {
         meshAddress = getIntent().getIntExtra(IntentConstant.MESH_ADDRESS, -1);
+        getDeviceType();
+
         List<BaseFragment> mFragmentList = new ArrayList<>();
         List<String> mTitleList = new ArrayList<>();
 
@@ -51,14 +56,23 @@ public class DeviceContentActivity extends BaseActivity {
         schedularFragment = new SchedularFragment();
 
         mFragmentList.add(sceneFragment);
-        mFragmentList.add(modesFragment);
-        mFragmentList.add(colourFragment);
-        mFragmentList.add(schedularFragment);
-
         mTitleList.add(getString(R.string.scenes));
-        mTitleList.add(getString(R.string.modes));
-        mTitleList.add(getString(R.string.colour));
+        mFragmentList.add(schedularFragment);
         mTitleList.add(getString(R.string.schedular));
+
+        if (deviceType == 1){
+            mFragmentList.add(modesFragment);
+            mTitleList.add(getString(R.string.modes));
+        }else if (deviceType == 2){
+            mFragmentList.add(colourFragment);
+            mTitleList.add(getString(R.string.colour));
+        }else {
+            mFragmentList.add(modesFragment);
+            mFragmentList.add(colourFragment);
+            mTitleList.add(getString(R.string.modes));
+            mTitleList.add(getString(R.string.colour));
+        }
+
 
         ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(this.getSupportFragmentManager(), mFragmentList, mTitleList);
         viewPager.setAdapter(pagerAdapter);
@@ -68,5 +82,22 @@ public class DeviceContentActivity extends BaseActivity {
     @OnClick(R.id.img_back)
     public void onBack(){
         finish();
+    }
+
+    // 0 二路 1 三路 2五路
+    private int deviceType;
+    private void getDeviceType(){
+        if (meshAddress < 0x8000) {
+            Device device = CoreData.core().mDeviceSparseArray.get(meshAddress);
+            if ((device.mDevType >> 8) == DeviceChannel.LIGHT_WC_TWO_CHANNEL ||
+                    (device.mDevType >> 8) == DeviceChannel.LIGHT_ONE_W_CHANNEL ||
+                    (device.mDevType >> 8) == DeviceChannel.LIGHT_ONE_C_CHANNEL) {
+                deviceType = 0;
+            } else if (device.mDevType >> 8 == DeviceChannel.LIGHT_THREE_RGB_CHANNEL){
+                deviceType = 1;
+            }else {
+                deviceType = 2;
+            }
+        }
     }
 }
